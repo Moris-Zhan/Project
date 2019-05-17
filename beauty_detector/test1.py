42ff344a62cfaf205bf4b1478a82072a673a2efb
Leyan529 <afly.bsky@gmail.com>
2019-05-18 01:04:10 +0800

    with another package

--- beauty_detector/test1.py
+++ beauty_detector/test1.py
@@ -7,23 +7,22 @@ from keras.preprocessing.image import load_img, img_to_array
 from keras.utils import np_utils
 from keras.applications.resnet50 import ResNet50
 from keras.callbacks import *
-from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import load_img
 from keras import backend as K
 from keras.models import load_model
+import tensorflow as tf
+import keras.backend.tensorflow_backend as KTF
 
 
 #%%
-import dlib
 import cv2
 import numpy as np
 import random
 import matplotlib.pyplot as plt
 import PIL
 from PIL import Image
-import imutils
-from imutils.face_utils import *
+
 import os
 from os import listdir
 import pickle
@@ -33,19 +32,11 @@ import time
 
 import face_recognition
 
-from imutils.face_utils import FaceAligner
-from imutils.face_utils import rect_to_bb
 # % matplotlib inline
 import os
 from os import listdir,getcwd
-import shutil
 
-#%%
-global detector ,landmark_predictor
-#宣告臉部偵測器，以及載入預訓練的臉部特徵點模型
-detector = dlib.get_frontal_face_detector()
-landmark_predictor = dlib.shape_predictor('./face_model/shape_predictor_68_face_landmarks.dat')
-ㄍ
+
 #%% 臉部識別函數宣告
 #讀取評分數據
 rating_dict={}
@@ -56,28 +47,6 @@ with open('./All_labels.txt','rb') as label:
         rating_dict[d[0]] = float(d[1])        
 
 def get_face(fullpath,pred=True):
-    #產生臉部識別
-    # face_rects = detector(img, 1)
-    # for i, d in enumerate(face_rects):
-    #     #讀取框左上右下座標
-    #     x1 = d.left()
-    #     y1 = d.top()
-    #     x2 = d.right()
-    #     y2 = d.bottom()
-    #     #根據此座標範圍讀取臉部特徵點
-    #     shape = landmark_predictor(img, d)
-    #     #將特徵點轉為numpy
-    #     shape = shape_to_np(shape)# (68,2)    
-    #     # 透過dlib挖取臉孔部分，將臉孔圖片縮放至256*256的大小，並存放於pickle檔中
-    #     # 人臉圖像部分呢。很簡單，只要根據畫框的位置切取即可crop_img = img[y1:y2, x1:x2, :]
-    #     crop_img = img[y1:y2, x1:x2, :]   
-    #     try:
-    #         crop_img = cv2.resize(crop_img, (128, 128))         
-    #         return crop_img   
-    #     except:
-    #         return np.array([0])  
-
-
     img = face_recognition.load_image_file(fullpath)
     # (Top,Left,Buttom,right)
     locat = face_recognition.face_locations(img,model="cnn")
@@ -175,7 +144,13 @@ seed = 1
 x_train_all, x_test, y_train_all, y_test = train_test_split(train_x, np.array(train_y), test_size=0.2, random_state=seed)
 x_train, x_val, y_train, y_val = train_test_split(x_train_all, y_train_all, test_size=0.2, random_state=seed)
 #%%
-
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+# config = tf.ConfigProto()
+# # config.gpu_options.allow_growth=True
+# config.gpu_options.per_process_gpu_memory_fraction = 0.5
+# sess = tf.Session(config=config)
+# KTF.set_session(sess)
+#%%
 def root_mean_squared_error(y_true, y_pred):
         return K.sqrt(K.mean(K.square(y_pred - y_true), axis=-1)) 
 
@@ -187,7 +162,7 @@ def make_network():
     model.layers[0].trainable = True
 #     model.compile(loss='mse', optimizer='adam')    
     # model.compile(loss='mse', optimizer='Adam',metrics=['accuracy'])
-    model.compile(loss = root_mean_squared_error, optimizer='rmsprop',metrics=['accuracy'])
+    model.compile(loss = root_mean_squared_error, optimizer='Adam',metrics=['accuracy'])
     model.summary()
     model.summary()
     return model
@@ -197,7 +172,7 @@ model = make_network()
 #%% 模型訓練
 early_stopping = EarlyStopping(
     monitor='val_loss', 
-    patience = 5, 
+    patience = 10, 
     verbose = 0, 
     mode='auto'
 )
@@ -211,12 +186,12 @@ reduce_learning_rate = ReduceLROnPlateau(monitor='loss',
                                          min_lr=0.00001,
                                          verbose=1)
 
-# callback_list = [checkpoint, reduce_learning_rate,early_stopping]
+callback_list = [checkpoint, reduce_learning_rate,early_stopping]
 
-callback_list = [checkpoint,early_stopping]
+# callback_list = [checkpoint,early_stopping]
 
-train_history = model.fit(x_train, y_train, 
-                          batch_size=16, epochs=30, verbose=1, 
+train_history = model.fit(x_train, y_train,                           
+                          batch_size=8, epochs=50, verbose=1, 
                           validation_split=0.2,
                           validation_data=(x_val, y_val),
                           callbacks = callback_list)
@@ -246,7 +221,8 @@ model.save('face_model/faceRank.h5')
 model.save_weights('face_model/faceRank_weights.h5')
 del model
 
-model = load_model('face_model/faceRank.h5')
+model = load_model('face_model/faceRank.h5',
+ custom_objects={'root_mean_squared_error': root_mean_squared_error})
 model.load_weights('face_model/faceRank_weights.h5')
 
 #%% 目標顏值預測
